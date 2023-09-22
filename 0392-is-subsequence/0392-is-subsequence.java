fbea383643a1ce48abc792d1730c91742cf353fd
Kavi Kandpal <kavikandpal123456@gmail.com>
2023-09-22 18:02:13 +0530

    Time: 1 ms (89.69%), Space: 40.5 MB (38.9%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -5,12 +5,12 @@ class Solution {
         }
         int j=0;
         int i=0;
-        int count=0;
+        // int count=0;
         while(i<s.length() && j<t.length()){
             if(s.charAt(i)==t.charAt(j)){
                 i++;
                 j++;
-                count++;
+                // count++;
                 
             }
             else{
