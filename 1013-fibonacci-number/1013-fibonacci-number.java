d0e97904ec7610547ae976a5c252844dd4a264f5
Kavi Kandpal <kavikandpal123456@gmail.com>
2023-09-24 18:35:00 +0530

    Time: 0 ms (100%), Space: 39.6 MB (16.75%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,8 +1,15 @@
 class Solution {
     public int fib(int n) {
-        // return fib(n)+fib(n-1);
-        if (n <= 1)
-            return n;
-        return fib(n - 1) + fib(n - 2);
+        int p1 = 0, p2 = 1, num = 0;
+        if (n == 0) return 0;
+        else if (n==1) return 1;
+        else {
+            while(n-->1){
+                num = p1 + p2;
+                p1 = p2;
+                p2 = num;
+            }
+        }
+        return num;
     }
 }
